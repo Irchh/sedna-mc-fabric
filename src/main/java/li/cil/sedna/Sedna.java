65594e815970f8bb39bcd20c59862b10d428d523
Irjan Vuckovic Olsen <irjan.olsen@gmail.com>
2025-05-02 14:11:41 +0200

    Initial commit

---
+++ src/main/java/li/cil/sedna/Sedna.java
@@ -0,0 +1,15 @@
+package li.cil.sedna;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sedna implements ModInitializer {
+	public static final String MOD_ID = "sedna";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+		LOGGER.info("sedna-mc is installed");
+	}
+}
